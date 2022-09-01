4bb090cca50eee4f2f2fff703c2b063991da756e
kunalkatara12 <kunalkatara888@gmail.com>
2022-09-01 22:37:33 +0530

    added components , changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box  } from "@mui/material";
 const App = () => {
-  return (
-    <div>App</div>
-  )
+ <BrowserRouter>
+<Box sx={{backgroundColor:'#000'}}>
+  <NavBar>
+    <Routes>
+      <Route path='/' exact element={<Feed/>}></Route>
+      <Route path='/video/:id' exact element={<VideoDetail/>}></Route>
+      <Route path='/channel/:id' exact element={<ChannelDetail/>}></Route>
+      <Route path='/search/:searchTerm' exact element={<SearchFeed/>}></Route>
+    </Routes>
+  </NavBar>
+</Box>
+ </BrowserRouter>
 }
 
 export default App
