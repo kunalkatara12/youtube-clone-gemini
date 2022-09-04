e818a55ad3567529b122e3d41983a57942cc3e5d
kunalkatara12 <kunalkatara888@gmail.com>
2022-09-04 16:44:22 +0530

    added Search bar and modified navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Box  } from "@mui/material";
-const App = () => {
- <BrowserRouter>
-<Box sx={{backgroundColor:'#000'}}>
-  <NavBar>
-    <Routes>
-      <Route path='/' exact element={<Feed/>}></Route>
-      <Route path='/video/:id' exact element={<VideoDetail/>}></Route>
-      <Route path='/channel/:id' exact element={<ChannelDetail/>}></Route>
-      <Route path='/search/:searchTerm' exact element={<SearchFeed/>}></Route>
-    </Routes>
-  </NavBar>
-</Box>
- </BrowserRouter>
-}
+import { Box } from "@mui/material";
+import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+  Navbar,
+  Feed,
+} from "./components";
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Feed />}></Route>
+          <Route path="/video/:id" exact element={<VideoDetail />}></Route>
+          <Route path="/channel/:id" exact element={<ChannelDetail />}></Route>
+          <Route
+            path="/search/:searchTerm"
+            exact
+            element={<SearchFeed />}
+          ></Route>
+        </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { IconButton, Paper } from "@mui/material";
+import { borderRadius } from "@mui/system";
+// import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+import Search from "@mui/icons-material/Search";
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search"
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+       <Search/>
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
