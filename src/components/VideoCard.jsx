e10b994073d1ae25e68daa7a04396af5659af6e0
kunalkatara12 <kunalkatara888@gmail.com>
2022-10-23 20:32:39 +0530

    changes in VideoCard 23-10-2022 20:33

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,16 +1,29 @@
-import { Card, CardMedia } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, CardContent, CardMedia } from "@mui/material";
+import {
+  demoThumbnailUrl,
+  demoChannelTitle,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoVideoUrl,
+} from "../utils/constants";
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => (
+  <Card>
+    <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+      <CardMedia
+        image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+        alt={snippet?.title}
+        sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
+      />
+    </Link>
+    <CardContent sx={{backgroundColor:"#1e1e1e", height:"106px"}}/>
+  </Card>
+);
 
-const VideoCard = ({video:{id:{videoId},snippet}}) => {
-  return (
-    // console.log(videoId, snippet)
-    <Card>
-      <Link>
-      <CardMedia image={snippet?.thumbnails?.high?.url}/>
-      </Link>
-    </Card>
-  )
-}
-
-export default VideoCard
+export default VideoCard;
