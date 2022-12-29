32cf6699066cd29cd7fa39628dc925ab1d5dbb13
kunalkatara12 <kunalkatara888@gmail.com>
2022-12-29 14:49:09 +0530

    new changes deployed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "Men of Culture", icon: <CodeIcon /> },
+  { name: "Marvel Entertainment", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
